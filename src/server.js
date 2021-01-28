76aab7ed4b0ca8124504d1764b377d143b3a3814
Thays Silva Santos <72770843+Thays3209@users.noreply.github.com>
2021-01-28 20:08:37 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import mysql from 'mysql';
+
+const server = express();
+
+server.use (express.json());
+server.use(cors());
+
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "fseletro",
+})
+
+
+server.get("/produtos",(req,res) => {
+    conexao.query ("SELECT * FROM prod", (erro,result) => {
+        if (erro){
+            res.send(erro)
+        } else {
+            res.send(result)
+        }
+    })
+});
+
+server.listen(3333);
+console.log('Sevidor:OK');
